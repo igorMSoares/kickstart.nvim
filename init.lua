daa48c58697a825c90834b44db729452439825ed
igorMSoares <igorms.dev@gmail.com>
2024-07-06 14:42:04 -0300

    lsp and format for templ files

--- init.lua
+++ init.lua
@@ -539,7 +539,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
-        gopls = {},
+        gopls = { filetypes = { 'go', 'templ' } },
         pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -556,6 +556,8 @@ require('lazy').setup({
         tailwindcss = {},
         htmx = {},
         templ = {},
+        dockerls = {},
+        docker_compose_language_service = {},
         --
 
         lua_ls = {
@@ -642,6 +644,8 @@ require('lazy').setup({
         -- is found.
         javascript = { { 'prettier' } },
         typescript = { { 'prettier' } },
+        html = { { 'prettier' } },
+        templ = { { 'templ' } },
       },
       formatters = {
         prettier = {
