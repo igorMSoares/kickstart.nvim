229d08a76119d8c92f5be78951ffc85206b87701
igorMSoares <igorms.dev@gmail.com>
2024-11-10 18:24:52 -0300

    changes harpoon add buffer keymap

--- lua/custom/configs/harpoon.lua
+++ lua/custom/configs/harpoon.lua
@@ -2,7 +2,7 @@ local mark = require 'harpoon.mark'
 local ui = require 'harpoon.ui'
 
 vim.keymap.set('n', '<leader>a', mark.add_file)
-vim.keymap.set('n', '<C-a>', ui.toggle_quick_menu)
+vim.keymap.set('n', '<C-\\>', ui.toggle_quick_menu)
 
 vim.keymap.set('n', ',', function()
   ui.nav_file(1)
