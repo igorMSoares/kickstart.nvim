ddcd172adeb9733277697e219a8d2cad373d75b1
igorMSoares <igorms.dev@gmail.com>
2024-11-10 19:02:36 -0300

    fix: tsserver -> ts_ls

--- init.lua
+++ init.lua
@@ -597,7 +597,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        ts_ls = {},
         html = { filetypes = { 'html', 'templ' } },
         emmet_language_server = { filetypes = { 'html', 'templ' } },
         volar = {},
